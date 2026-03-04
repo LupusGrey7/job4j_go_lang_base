7ef0ab3c2f9571ae6da24b48aa7dcdf017e0f28b
lupusGrey7 <lupusGrey7@project.com>
2026-03-04 17:44:22 +0500

    feat: tsk-505568, DB connection files;
    #comment
    - added DB connection infrastructure;
    - added repository;

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"job4j.ru/go-lang-base/internal/base"
+	"job4j.ru/go-lang-base/internal/config"
+	"job4j.ru/go-lang-base/internal/db"
+	"job4j.ru/go-lang-base/internal/repository"
 	"job4j.ru/go-lang-base/internal/tracker"
+	"log"
 )
 
 func main() {
@@ -14,10 +19,33 @@ func main() {
 
 	fmt.Printf("%d + %d = %d\n", first, second, res)
 
-	ui := tracker.UI{
-		In:      tracker.ConsoleInput{},
-		Out:     tracker.ConsoleOutput{},
-		Tracker: tracker.NewTracker(),
+	//Tracker UI
+	ctx := context.Background()
+
+	cfg := db.Config{
+		Host:     config.Env("DB_HOST", "localhost"),
+		Port:     config.EnvInt("DB_PORT", 6543),
+		User:     config.Env("DB_USER", "postgres"),
+		Password: config.Env("DB_PASSWORD", "password"),
+		DBName:   config.Env("DB_NAME", "tracker"),
+		SSLMode:  config.Env("DB_SSLMODE", "disable"),
 	}
-	ui.Run()
+
+	pool, err := db.NewPool(ctx, cfg.DSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+
+	repo := repository.NewRepoPg(pool)
+
+	ui := tracker.UI{
+		In:    tracker.ConsoleInput{},
+		Out:   tracker.ConsoleOutput{},
+		Store: repo,
+	}
+
+	if err := ui.Run(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
